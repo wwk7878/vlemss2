158b51fd85b3205c6088fc527b81346abb96c343
wwk7878 <53973331+wwk7878@users.noreply.github.com>
2023-08-03 23:56:04 +0800

    app_edtunnel.js

--- app_edtunnel.js
+++ app_edtunnel.js
@@ -4,7 +4,7 @@ const net = require('net');
 
 // 创建 WebSocket 服务器
 const wss = new WebSocket.Server({ port: 443 });
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = 'ae420900-e12d-4b56-8c12-81ceb693f603';
 let proxyIP = "64.68.192." + Math.floor(Math.random() * 255);
 
 let address = '';
